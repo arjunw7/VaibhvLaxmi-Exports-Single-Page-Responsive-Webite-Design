57d66fee6cffd2622cc7d24e4a51324c2459326a
Arjun Wadhwa <arjunw7@gmail.com>
2016-03-06 08:24:18 +0530

    update 2

--- js/default.js
+++ js/default.js
@@ -2,36 +2,10 @@
 
 $(document).ready(function(){
 	jQuery(window).load(function() {
-	        // will first fade out the loading animation
 		jQuery("#status").fadeOut();
-	        // will fade out the whole DIV that covers the website.
 		jQuery("#preloader").delay(1000).fadeOut(500);
-	})
+	});
 
-	$(".section1").on({
-		mouseenter: function(){
-			$("#overview").attr('src','../images/overviewWhite.png');
-		},
-		mouseleave: function(){
-			$("#overview").attr('src','../images/overview.png');
-		}
-	});
-	$(".section2").on({
-		mouseenter: function(){
-			$("#outreach").attr('src','../images/outreachWhite.png');
-		},
-		mouseleave: function(){
-			$("#outreach").attr('src','../images/outreach.png');
-		}
-	});
-	$(".section3").on({
-		mouseenter: function(){
-			$("#strategy").attr('src','../images/strategyWhite.png');
-		},
-		mouseleave: function(){
-			$("#strategy").attr('src','../images/strategy.png');
-		}
-	});
 	(function($) {
 		"use strict";
 		function count($this){
@@ -41,7 +15,7 @@ $(document).ready(function(){
 			if(current > $this.data('count')){
 				$this.html($this.data('count'));
 			} else {    
-				setTimeout(function(){count($this)}, 50);
+				setTimeout(function(){count($this)}, 300);
 			}
 		}        	
 		$(".stat-count").each(function() {
@@ -50,6 +24,7 @@ $(document).ready(function(){
 		  count($(this));
 		});
    })(jQuery);
+
    $(".sectionContact input").focusin(function(){
    	$(this).css("border-width", "0px 3px 0px 6px");
    	$(this).css("background-color", "rgba(255,255,255,.9)");
