a6dc684b46cf345db4c00ce6b32ed9dca6aceadc
Arjun Wadhwa <arjunw7@gmail.com>
2016-03-20 00:05:42 +0530

    final update

--- js/default.js
+++ js/default.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
 		  count($(this));
 		});
    })(jQuery);
-
+   $(".live-tile").liveTile();
    $(".sectionContact input").focusin(function(){
    	$(this).css("border-width", "0px 3px 0px 6px");
    	$(this).css("background-color", "rgba(255,255,255,.9)");
@@ -44,77 +44,62 @@ $(document).ready(function(){
 
 
    $(".machine1").mouseenter(function(){
-   	$(this).css("width","60%");
-      $(".machine2").css("width","9.8%");
-   	$(".machine3").css("width","9.8%");
-   	$(".machine4").css("width","9.8%");
-   	$(".machine5").css("width","9.8%");
+   	$(this).css("width","58%");
+      $(".machine2, .machine3, .machine4, .machine5").css("width","10%");
+      $(".desDetail2, .desDetail3, .desDetail4, .desDetail5").hide(250);
    });
    $(".machine1").mouseleave(function(){
-   	$(this).css("width","20%");
-   	$(".machine2").css("width","19.8%");
-   	$(".machine3").css("width","19.8%");
-   	$(".machine4").css("width","19.8%");
-   	$(".machine5").css("width","19.8%");
+   	$(this).css("width","19.6%");
+      $(".machine2, .machine3, .machine4, .machine5").css("width","19.6%");
+      $(".desDetail2, .desDetail3, .desDetail4, .desDetail5").show(250);
    });
 
     $(".machine2").mouseenter(function(){
-   	$(this).css("width","60%");
-   	$(".machine1").css("width","9.8%");
-   	$(".machine3").css("width","9.8%");
-   	$(".machine4").css("width","9.8%");
-   	$(".machine5").css("width","9.8%");
+   	$(this).css("width","58%");
+   	$(".machine1, .machine3, .machine4, .machine5").css("width","10%");
+      $(".desDetail1, .desDetail3, .desDetail4, .desDetail5").hide(250);
    });
    $(".machine2").mouseleave(function(){
-   	$(this).css("width","20%");
-   	$(".machine1").css("width","19.8%");
-   	$(".machine3").css("width","19.8%");
-   	$(".machine4").css("width","19.8%");
-   	$(".machine5").css("width","19.8%");
+   	$(this).css("width","19.6%");
+   	 $(".machine1, .machine3, .machine4, .machine5").css("width","19.6%");
+      $(".desDetail1, .desDetail3, .desDetail4, .desDetail5").show(250);
    });
 
     $(".machine3").mouseenter(function(){
-   	$(this).css("width","60%");
-   	$(".machine1").css("width","9.8%");
-   	$(".machine2").css("width","9.8%");
-   	$(".machine4").css("width","9.8%");
-   	$(".machine5").css("width","9.8%");
+   	$(this).css("width","58%");
+   	$(".machine2, .machine1, .machine4, .machine5").css("width","10%");
+      $(".desDetail2, .desDetail1, .desDetail4, .desDetail5").hide(250);
    });
    $(".machine3").mouseleave(function(){
-   	$(this).css("width","20%");
-   	$(".machine1").css("width","19.8%");
-   	$(".machine2").css("width","19.8%");
-   	$(".machine4").css("width","19.8%");
-   	$(".machine5").css("width","19.8%");
+   	$(this).css("width","19.6%");
+   	 $(".machine2, .machine1, .machine4, .machine5").css("width","19.6%");
+      $(".desDetail2, .desDetail1, .desDetail4, .desDetail5").show(250);
    });
 
     $(".machine4").mouseenter(function(){
-   	$(this).css("width","60%");
-   	$(".machine1").css("width","9.8%");
-   	$(".machine2").css("width","9.8%");
-   	$(".machine3").css("width","9.8%");
-   	$(".machine5").css("width","9.8%");
+   	$(this).css("width","58%");
+   	$(".machine2, .machine3, .machine1, .machine5").css("width","10%");
+      $(".desDetail2, .desDetail3, .desDetail1, .desDetail5").hide(250);
    });
    $(".machine4").mouseleave(function(){
-   	$(this).css("width","20%");
-   	$(".machine1").css("width","19.8%");
-   	$(".machine2").css("width","19.8%");
-   	$(".machine3").css("width","19.8%");
-   	$(".machine5").css("width","19.8%");
+   	$(this).css("width","19.6%");
+   	 $(".machine2, .machine3, .machine1, .machine5").css("width","19.6%");
+      $(".desDetail2, .desDetail3, .desDetail1, .desDetail5").show(250);
    });
 
     $(".machine5").mouseenter(function(){
-   	$(this).css("width","60%");
-   	$(".machine1").css("width","9.8%");
-   	$(".machine2").css("width","9.8%");
-   	$(".machine3").css("width","9.8%");
-   	$(".machine4").css("width","9.8%");
+   	$(this).css("width","58%");
+   	$(".machine2, .machine3, .machine4, .machine1").css("width","10%");
+      $(".desDetail2, .desDetail3, .desDetail4, .desDetail1").hide(250);
    });
    $(".machine5").mouseleave(function(){
-   	$(this).css("width","20%");
-   	$(".machine1").css("width","19.8%");
-   	$(".machine2").css("width","19.8%");
-   	$(".machine3").css("width","19.8%");
-   	$(".machine4").css("width","19.8%");
+   	$(this).css("width","19.6%");
+   	 $(".machine2, .machine3, .machine4, .machine1").css("width","19.6%");
+      $(".desDetail2, .desDetail3, .desDetail4, .desDetail1").show(250);
    });
+
+
 });
+
+//Usage w/options:
+$().framerate({framerate: 24, logframes: true})
